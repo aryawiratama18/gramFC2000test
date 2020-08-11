b4a6a0c334382b6d2941b8648415ad777dd790fa
aryawiratama18 <NAwiratama@gmail.com>
2020-08-11 19:48:45 +0700

    Add connect button

--- serial.js
+++ serial.js
@@ -1,85 +1,126 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 const serialport = require("serialport");
-
+let baudRateValue;
 
 serialport.list((err, ports) => {
-  console.log('ports', ports);
-  if (err) {
-    console.log(err.message);
-    return
-  } else {
-    console.log('');
-  }
-
-  if (ports.length === 0) {
-    console.log('No ports discovered');
-  }
-
-
+  console.log("ports", ports);
   let port = new serialport(ports[0].comName, {
     baudRate: 9600,
   });
-  let autoscroll = document.getElementById("autoscroll");
   let dataArr = [];
+  let autoscroll = document.getElementById("autoscroll");
 
-  port.on("open", () => {
-    console.log("Port opened");
+  port.close((err) => {
+    console.log("Port Closed", err);
   });
-  port.on("data", (data) => {
-    let dataSerial = data.toString("utf-8");
-    let dataMonitorContainer = document.getElementById("serial-data");
-    let dataMonitorLine = document.createElement("div");
 
-    dataArr.push(dataSerial);
-    console.log(dataArr);
-    let lastArr = dataArr.slice(-1).pop();
-    let dateAndData = dateTimeStamp() + '  ' + lastArr;
-    let dataDisplayed = document.createTextNode(dateAndData);
-
-    // if autoscroll checkbox is checked
-    if (autoscroll.checked == true) {
-      if (dataArr.length > 20) {
-        dataMonitorLine.appendChild(dataDisplayed);
-        dataMonitorContainer.appendChild(dataMonitorLine).scrollIntoView();
-
-      } else if (dataArr.length > 0) {
-        let lastArr = dataArr.slice(-1).pop();
-        dateAndData = dateTimeStamp() + '  ' + lastArr;
-        dataDisplayed = document.createTextNode(dateAndData);
-        dataMonitorLine.appendChild(dataDisplayed);
-        dataMonitorContainer.appendChild(dataMonitorLine);
-      } else {
-        dataMonitorLine.innerHTML = dateTimeStamp() + '  ' + "No incoming data";
-        dataMonitorContainer.appendChild(dataMonitorLine);
+  // Connection button
+  let connButton = document.querySelector("#conn-button");
+  connButton.addEventListener("click", () => {
+    if (connButton.innerHTML === "Connect") {
+      connButton.innerHTML = "Disconnect";
+      // isConnected = true;
+      if (ports.length === 0) {
+        console.log("No ports discovered");
       }
+      port.open(() => {
+        port.on("data", (data) => {
+          let dataSerial = data.toString("utf-8");
+          let dataMonitorContainer = document.getElementById("serial-data");
+          let dataMonitorLine = document.createElement("div");
+
+          dataArr.push(dataSerial);
+          console.log(dataArr);
+
+          let lastArr = dataArr.slice(-1).pop();
+          let dateAndData = dateTimeStamp() + "  " + lastArr;
+          let dataDisplayed = document.createTextNode(dateAndData);
+
+          // if autoscroll checkbox is checked
+          if (autoscroll.checked == true) {
+            if (dataArr.length > 20) {
+              dataMonitorLine.appendChild(dataDisplayed);
+              dataMonitorContainer
+                .appendChild(dataMonitorLine)
+                .scrollIntoView();
+            } else if (dataArr.length > 0) {
+              let lastArr = dataArr.slice(-1).pop();
+              dateAndData = dateTimeStamp() + "  " + lastArr;
+              dataDisplayed = document.createTextNode(dateAndData);
+              dataMonitorLine.appendChild(dataDisplayed);
+              dataMonitorContainer.appendChild(dataMonitorLine);
+            } else {
+              dataMonitorLine.innerHTML =
+                dateTimeStamp() + "  " + "No incoming data";
+              dataMonitorContainer.appendChild(dataMonitorLine);
+            }
+          } else {
+            if (dataArr.length > 20) {
+              let lastArr = dataArr.slice(-1).pop();
+              dateAndData = dateTimeStamp() + "  " + lastArr;
+              dataDisplayed = document.createTextNode(dateAndData);
+              dataMonitorLine.appendChild(dataDisplayed);
+              dataMonitorContainer.appendChild(dataMonitorLine);
+            } else if (dataArr.length > 0) {
+              let lastArr = dataArr.slice(-1).pop();
+              dateAndData = dateTimeStamp() + "  " + lastArr;
+              dataDisplayed = document.createTextNode(dateAndData);
+              dataMonitorLine.appendChild(dataDisplayed);
+              dataMonitorContainer.appendChild(dataMonitorLine);
+            } else {
+              dataMonitorLine.innerHTML =
+                dateTimeStamp() + "  " + "No incoming data";
+              dataMonitorContainer.appendChild(dataMonitorLine);
+            }
+          }
+        });
+      });
     } else {
-      if (dataArr.length > 20) {
-        let lastArr = dataArr.slice(-1).pop();
-        dataDisplayed = document.createTextNode(lastArr);
-        dataMonitorLine.appendChild(dataDisplayed);
-        dataMonitorContainer.appendChild(dataMonitorLine);
-      } else if (dataArr.length > 0) {
-        let lastArr = dataArr.slice(-1).pop();
-        dataDisplayed = document.createTextNode(lastArr);
-        dataMonitorLine.appendChild(dataDisplayed);
-        dataMonitorContainer.appendChild(dataMonitorLine);
-      } else {
-        dataMonitorLine.innerHTML = "No incoming data";
-        dataMonitorContainer.appendChild(dataMonitorLine);
-      }
+      // isConnected = false;
+      port.close((err) => {
+        console.log("Port Closed", err);
+      });
+      connButton.innerHTML = "Connect";
     }
   });
 });
 
+// Dropdown Baudrate
+let dropdownButton = document.querySelector(".dropbtn");
+dropdownButton.addEventListener("click", function () {
+  document.getElementById("baudRateMenu").classList.toggle("show");
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("baudRateMenu");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
+const baudRateMenu = document.getElementById("baudRateMenu");
+baudRateMenu.addEventListener("click", function (e) {
+  if (e.target.className == "baud") {
+    document.getElementById("baud").forEach(function (b) {
+      if (b.classList.contains("active")) {
+        baudRateValue = b.innerHTML;
+      } else {
+        b.target.classList.add("active");
+        baudRateValue = b.innerHTML;
+      }
+    });
+  }
+});
+
+// Date
 function dateTimeStamp() {
   var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   var yyyy = today.getFullYear();
 
-  today = dd + '/' + mm + '/' + yyyy;
+  today = dd + "/" + mm + "/" + yyyy;
   return today;
 }
