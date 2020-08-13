296880fe8f39efc9d8b93f20eafb6decca057f6b
aryawiratama18 <NAwiratama@gmail.com>
2020-08-13 20:13:39 +0700

    Add form insertion

--- database.js
+++ database.js
@@ -14,25 +14,33 @@ connection.connect((err) => {
   console.log("Mysql connected");
 });
 
-exports.registrasiFormBarang = (req, res) => {
-  var formRegistrasi = {
-    nama_barang: req.body.nama_barang,
-    tipe_barang: req.body.tipe_barang,
-    id_barang: req.body.id_barang,
-    tanggal_registrasi: new Date(),
-  };
-
-  var query = "INSERT INTO ?? SET ?";
-  var table = ["form_barang"];
-  query = mysql.format(query, table);
-  connection.query(query, formRegistrasi, (error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.ok("Berhasil menambahkan data barang");
+// Class Parser
+class TextParser {
+  constructor() {
+    this.string = "";
+  }
+  static isEndMarker(char) {
+    return char == "\r" || char == "\n"; // New line characters (NL & CR)
+  }
+  parse(char) {
+    if (this.clear) {
+      this.string = "";
+      this.clear = false;
     }
-  });
-};
+    if (TextParser.isEndMarker(char)) {
+      if (this.string.length > 0) {
+        this.clear = true;
+        return true;
+      }
+      return false;
+    } else {
+      this.string += char;
+    }
+  }
+  get message() {
+    return this.string;
+  }
+}
 
 // Fungsi untuk simpan data serial
 serialport.list((err, ports) => {
@@ -47,12 +55,31 @@ serialport.list((err, ports) => {
     console.log("Port Closed", err);
   });
 
+  var formRegistrasi = {
+    nama_barang: document.getElementById("nama_barang").value,
+    tipe_barang: document.getElementById("tipe_barang").value,
+    id_barang: document.getElementById("id_barang").value,
+    tanggal_registrasi: document.getElementById("tanggal_registrasi").value,
+  };
+
   // Tombol Record
-  let recordButton = document.querySelector("#rec-button");
+  let recordButton = document.getElementById("rec-button");
   recordButton.addEventListener("click", () => {
     // ketika tombol connect ditekan, maka akan mulai record data
     if (connButton.innerHTML === "Start Record") {
       connButton.innerHTML = "Stop Record";
+
+      var query = "INSERT INTO ?? SET ?";
+      var table = ["form_barang"];
+      query = mysql.format(query, table);
+      connection.query(query, formRegistrasi, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          response.ok("Berhasil menambahkan data barang");
+        }
+      });
+
       if (ports.length === 0) {
         console.log("No ports discovered");
       }
@@ -74,19 +101,30 @@ serialport.list((err, ports) => {
     }
   });
 
-  // Insert ke database.
-  if (dataArr.length != 0) {
-    // Jika array tidak kosong, isi dengan data array yang terakhir
-    connection.query(
-      "INSERT INTO data_timbangan (data) VALUE (?)",
-      [dataArr.slice(-1).pop()],
-      (error) => {
-        if (error) {
-          console.log(error);
-        } else {
-          response.ok("Sukses insert data");
+  const parser = new TextParser();
+  if (dataArr != 0) {
+    let dataArr = dataArr.toString("utf-8"); // Konversi tipe karakter ke utf-8
+
+    for (let i = 0; i < dataArr.length; i++) {
+      for (let j = 0; j < dataArr[i].length; j++) {
+        if (parser.parse(dataArr[i][j])) {
+          insertIntoDatabase(parser.message);
         }
       }
-    );
+    }
   }
 });
+
+insertIntoDatabase = (value) => {
+  connection.query(
+    "INSERT INTO data_timbangan (data) VALUE (?)",
+    [value],
+    (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Sukses insert data");
+      }
+    }
+  );
+};
